54c469cb0b250ddf1da6c2bdd9bc8511938f3add
Guteberg Cruz <gute.tec@gmail.com>
2020-06-06 17:24:47 -0300

    commit

---
+++ server/src/routers.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
+
+const routes = express.Router();
+const upload = multer(multerConfig);
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', upload.single('image'), pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+export default routes;
---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routers';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.listen(3333);
